16b47a804ad9318bbd894bdc078d10d6998d5036
Alish142 <a.ranabhat.12@student.scu.edu.au>
2024-11-24 15:51:35 +1000

    Update app.routing.module.ts

--- app.routing.module.ts
+++ app.routing.module.ts
@@ -6,3 +6,20 @@ import { ClientSearchComponent } from './client-search/client-search.component';
 import { VipClientsComponent } from './vip-clients/vip-clients.component';
 import { PrivacySecurityComponent } from './privacy-security/privacy-security.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+
+const routes: Routes = [
+  { path: '', component: ClientListComponent },
+  { path: 'add-client', component: ClientFormComponent },
+  { path: 'edit-client/:id', component: ClientFormComponent },
+  { path: 'search', component: ClientSearchComponent },
+  { path: 'vip-clients', component: VipClientsComponent },
+  { path: 'privacy-security', component: PrivacySecurityComponent },
+  { path: '**', component: NotFoundComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
+
