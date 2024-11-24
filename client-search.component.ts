696c2c81e62cd0a7293239c8aa019e823b49f36b
Alish142 <a.ranabhat.12@student.scu.edu.au>
2024-11-24 16:52:39 +1000

    Update client-search.component.ts

--- client-search.component.ts
+++ client-search.component.ts
@@ -2,3 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { ClientDataService, Client } from '../client-data.service';
+
+
+@Component({
+  selector: 'app-client-search',
+  templateUrl: './client-search.component.html',
+  styleUrls: ['./client-search.component.css'],
+  standalone: true,
+  imports: [CommonModule, FormsModule]
+})
