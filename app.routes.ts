fdadfe9acfc05451d04323511f4c75b0468b6512
Alish142 <a.ranabhat.12@student.scu.edu.au>
2024-11-24 15:43:35 +1000

    Create app.routes.ts

---
+++ app.routes.ts
@@ -0,0 +1,8 @@
+import { Routes } from '@angular/router';
+import { ClientListComponent } from './client-list/client-list.component';
+import { ClientFormComponent } from './client-form/client-form.component';
+import { ClientSearchComponent } from './client-search/client-search.component';
+import { VipClientsComponent } from './vip-clients/vip-clients.component';
+import { PrivacySecurityComponent } from './privacy-security/privacy-security.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+
