5ce716d21bed7fec59bb1132a11de7e5b30eb8e0
Alish142 <a.ranabhat.12@student.scu.edu.au>
2024-11-24 15:39:37 +1000

    Update client-form.component.ts

--- client-form.component.ts
+++ client-form.component.ts
@@ -13,3 +13,22 @@ import { ClientDataService, Client } from '../client-data.service';
 export class ClientFormComponent implements OnInit {
   clientForm: FormGroup;
   isEditMode = false;
+  
+constructor(
+    private fb: FormBuilder,
+    private clientDataService: ClientDataService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {
+    this.clientForm = this.fb.group({
+      clientID: ['', Validators.required],
+      name: ['', Validators.required],
+      dob: ['', Validators.required],
+      gender: ['', Validators.required],
+      fitnessProgram: ['', Validators.required],
+      contactInfo: ['', Validators.required],
+      joinedDate: ['', Validators.required],
+      endingDate: ['', Validators.required],
+      specialHealthNotes: [''],
+      isVIP: [false]
+    });
