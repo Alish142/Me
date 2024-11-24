9deb9db3f465f2d756dccc3c33b8279ada54ac21
Alish142 <a.ranabhat.12@student.scu.edu.au>
2024-11-24 17:13:09 +1000

    Update vip-clients.component.ts

--- vip-clients.component.ts
+++ vip-clients.component.ts
@@ -2,4 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ClientDataService, Client } from '../client-data.service';
 
-
+@Component({
+  selector: 'app-vip-clients',
+  templateUrl: './vip-clients.component.html',
+  styleUrls: ['./vip-clients.component.css'],
+  standalone: true,
+  imports: [CommonModule]
+})
