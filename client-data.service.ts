340b355534be347b2df14d3a2532a2738c3de070
Alish142 <a.ranabhat.12@student.scu.edu.au>
2024-11-24 15:54:03 +1000

    Create client-data.service.ts

---
+++ client-data.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+
+export interface Client {
+  clientID: string;
+  name: string;
+  dob: string;
+  gender: string;
+  fitnessProgram: string;
+  contactInfo: string;
+  joinedDate: string;
+  endingDate: string;
+  specialHealthNotes: string;
+  isVIP: boolean;
+}
