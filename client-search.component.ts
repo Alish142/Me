098e596371865603c966f847ae1503a8b2d3261e
Alish142 <a.ranabhat.12@student.scu.edu.au>
2024-11-24 16:50:05 +1000

    Update client-search.component.ts

--- client-search.component.ts
+++ client-search.component.ts
@@ -1,1 +1,4 @@
-
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { ClientDataService, Client } from '../client-data.service';
